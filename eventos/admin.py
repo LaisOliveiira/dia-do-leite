8527ae799f0ae0a07d03cd0fcc7c2f903b94856e
LaisOliveiira <laisdepaula163@gmail.com>
2026-05-11 18:39:42 -0300

    feat: cria models, views e template interativo com abas para a Galeria de Edicoes

--- eventos/admin.py
+++ eventos/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import TipoEvento, Evento, Inscricao
+from .models import Edicao, GaleriaFoto
 
 admin.site.register(TipoEvento)
 admin.site.register(Evento)
 admin.site.register(Inscricao)
+
+class GaleriaInline(admin.TabularInline):
+    model = GaleriaFoto
+    extra = 3
+
+@admin.register(Edicao)
+class EdicaoAdmin(admin.ModelAdmin):
+    inlines = [GaleriaInline]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -9,12 +9,5 @@ urlpatterns = [
     path('perfil/', views.perfil_view, name='perfil'),
     path('certificados/', views.certificados_view, name='certificados'),
     path('gerar-pdf/<int:inscricao_id>/', views.gerar_certificado_pdf, name='gerar_certificado_pdf'),
-    path('certificados/', views.certificados_view, name='certificados'),
-    path('ativar/<uidb64>/<token>/', views.ativar_conta_view, name='ativar_conta'),
-    
-    # A nova rota do PDF (repare na vírgula no final!)
-    path('gerar-pdf/<int:inscricao_id>/', views.gerar_certificado_pdf, name='gerar_certificado_pdf'),
-    
-    # NOVA ROTA: O link mágico que vai no e-mail
     path('ativar/<uidb64>/<token>/', views.ativar_conta_view, name='ativar_conta'),
 ]
